6c7ef629719259eab83195a3c2d535761c5fae11
Ko10PEACE <coltonpierce17@gmail.com>
2023-01-31 09:43:09 -0700

    Create server.js
    Boilerplate

---
+++ server.js
@@ -0,0 +1,25 @@
+// //Server Files
+
+// const express = require("express");
+
+// const app = express();
+
+// //specifies what happens when a browser makes a request of the server
+// //app.get("Page name, ") 
+// app.get("/", function(req, res) {
+//     res.send(<h1>"Hello World"</h1>);
+//     console.log(req);
+// });
+
+// app.get("/contact", function(req, res){
+//     res.send("Contact me at: fake@gmail.com");
+// });
+
+// app.get("/about", function(req, res){
+//     res.send("I am all about the programming life");
+// });
+
+// //adds a listeneer for (port number)
+// app.listen(3000, function() {
+//     console.log("Server started on port 3000");
+// };
